6969433fcbdb659066846f2f649bfc6c848fb1e1
Damian <damian.bartnowski@gmail.com>
2021-08-22 19:27:04 +0200

    alert configuration

--- APIUser/ViewController.swift
+++ APIUser/ViewController.swift
@@ -20,6 +20,44 @@ class ViewController: UIViewController {
     
     }
     
+    func showResultAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let startAction = UIContextualAction(style: .normal, title: "Star") { (action, view, completion) in
+        
+        completion(true)
+        }
+        let unstarAction = UIContextualAction(style: .normal, title: "Unstar") { (action, view, completion) in
+            
+            completion(true)
+        }
+        
+        
+        startAction.backgroundColor = .blue
+        unstarAction.backgroundColor = .darkGray
+        
+        let actionConfig = UISwipeActionsConfiguration(actions: [startAction, unstarAction])
+        return actionConfig
+    }
+    
+}
